c7111a204d67720b429c8cf64e077f77b8f4f766
Chong <chongwan.w@gmail.com>
2024-03-22 14:32:03 -0400

    recommend locations

--- pomodoro.py
+++ pomodoro.py
@@ -1,10 +1,74 @@
-from tkinter import *
-# https://www.youtube.com/watch?v=FpZ70RkUfNs
-import math
+"""The source code for the pomodoro app, source code borrowed and modified from
+https://github.com/achudnova/projects-yt/blob/main/Pomodoro/pomodoro.py"""
+import tkinter as tk
+from tkinter import messagebox
+from ttkbootstrap import ttk, Style
 
-def reset_timer():
-    window.after_cancel(timer)
-    canvas.itemconfig(timer_text, text = '00:00')
-    title_label.config(text = 'Timer')
+# Set the default time for work and break intervals
+WORK_TIME = 25 * 60
+SHORT_BREAK_TIME = 5 * 60
+LONG_BREAK_TIME = 15 * 60
 
-u
+class PomodoroTimer:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("200x200")
+        self.root.title("Pomodoro Timer")
+        self.style = Style(theme="simplex")
+        self.style.theme_use()
+
+        self.timer_label = tk.Label(self.root, text="", font=("TkDefaultFont", 40))
+        self.timer_label.pack(pady=20)
+
+        self.start_button = ttk.Button(self.root, text="Start", command=self.start_timer)
+        self.start_button.pack(pady=5)
+
+        self.stop_button = ttk.Button(self.root, text="Stop", command=self.stop_timer,
+                                      state=tk.DISABLED)
+        self.stop_button.pack(pady=5)
+
+        self.work_time, self.break_time = WORK_TIME, SHORT_BREAK_TIME
+        self.is_work_time, self.pomodoros_completed, self.is_running = True, 0, False
+
+        self.root.mainloop()
+
+    def start_timer(self):
+        self.start_button.config(state=tk.DISABLED)
+        self.stop_button.config(state=tk.NORMAL)
+        self.is_running = True
+        self.update_timer()
+
+    def stop_timer(self):
+        self.start_button.config(state=tk.NORMAL)
+        self.stop_button.config(state=tk.DISABLED)
+        self.is_running = False
+
+    def update_timer(self):
+        if self.is_running:
+            if self.is_work_time:
+                self.work_time -= 1
+                if self.work_time == 0:
+                    self.is_work_time = False
+                    self.pomodoros_completed += 1
+                    self.break_time = LONG_BREAK_TIME if self.pomodoros_completed % 4 == 0 else SHORT_BREAK_TIME
+                    messagebox.showinfo("Great job!" if self.pomodoros_completed % 4 == 0
+                                        else "Good job!", "Take a long break and rest your mind."
+                                        if self.pomodoros_completed % 4 == 0
+                                        else "Take a short break and strech your legs!")
+            else:
+                self.break_time -= 1
+                if self.break_time == 0:
+                    self.is_work_time, self.work_time = True, WORK_TIME
+                    messagebox.showinfo("Work TIie", "Get back to work!")
+            minutes, seconds = divmod(self.work_time if self.is_work_time else self.break_time, 60)
+            self.timer_label.config(text="{:02d}:{:02d}".format(minutes, seconds))
+            self.root.after(1000, self.update_timer)
+
+
+if __name__ == '__main__':
+
+    WORK_TIME = int(input("How long would you like to study for?: ")) * 60
+    SHORT_BREAK_TIME = int(input("How long would you like your short break to be?: ")) * 60
+    LONG_BREAK_TIME = int(input("How long would you like your long break to be?: ")) * 60
+
+    PomodoroTimer()
--- recommendation.py
+++ recommendation.py
@@ -3,112 +3,69 @@ from typing import Any, Optional
 
 
 class _Vertex:
-    """A vertex in a graph.
-
-    Instance Attributes:
-        - item: The data stored in this vertex.
-        - kind: The type of the vertex (location or student)
-        - neighbours: The vertices that are adjacent to this vertex.
-
-    Representation Invariants:
-        - self not in self.neighbours
-        - all(self in u.neighbours for u in self.neighbours)
-    """
     item: Any
     kind: str
     neighbours: set[_Vertex]
-    # neighbours: dict[_WeightedVertex, Union[int, float]]
 
-    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
-        """Initialize a new vertex with the given item and neighbours."""
+    def __init__(self, item: Any, kind: str) -> None:
         self.item = item
-        self.neighbours = neighbours
+        self.kind = kind
+        self.neighbours = set()
 
-    def check_connected(self, target_item: Any, visited: set[_Vertex]) -> bool:
-        """Return whether this vertex is connected to a vertex corresponding to the target_item,
-        WITHOUT using any of the vertices in visited.
+    def degree(self) -> int:
+        return len(self.neighbours)
 
-        Preconditions:
-            - self not in visited
-        """
-        if self.item == target_item:
-            # Our base case: the target_item is the current vertex
-            return True
+    def similarity_score(self, other: _Vertex) -> float:
+        x = self.neighbours
+        y = other.neighbours
+        if self.degree() == 0 or other.degree() == 0:
+            return 0
         else:
-            visited.add(self)         # Add self to the set of visited vertices
-            for u in self.neighbours:
-                if u not in visited:  # Only recurse on vertices that haven't been visited
-                    if u.check_connected(target_item, visited):
-                        return True
-
-            return False
+            common = 0
+            a = 0
+            for vertex in x:
+                if vertex in y:
+                    common += 1
+                    a += 1
+                else:
+                    a += 1
+            for vertex in y:
+                if vertex in x:
+                    pass
+                else:
+                    a += 1
+            return common / a
 
 
 class Graph:
-    """A graph.
-
-    Representation Invariants:
-        - all(item == self._vertices[item].item for item in self._vertices)
-    """
-    # Private Instance Attributes:
-    #     - _vertices:
-    #         A collection of the vertices contained in this graph.
-    #         Maps item to _Vertex object.
     _vertices: dict[Any, _Vertex]
-    # _vertices: dict[Any, _WeightedVertex]
 
     def __init__(self) -> None:
         """Initialize an empty graph (no vertices or edges)."""
         self._vertices = {}
 
-    def add_vertex(self, item: Any) -> None:
-        """Add a vertex with the given item to this graph.
-
-        The new vertex is not adjacent to any other vertices.
-
-        Preconditions:
-            - item not in self._vertices
-        """
-        if item not in self._vertices:
-            self._vertices[item] = _Vertex(item, set())
-
-    def add_edge(self, item1: Any, item2: Any) -> None:
-        """Add an edge between the two vertices with the given items in this graph.
-
-        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
-
-        Preconditions:
-            - item1 != item2
-        """
-        if item1 in self._vertices and item2 in self._vertices:
-            v1 = self._vertices[item1]
-            v2 = self._vertices[item2]
-
-            # Add the new edge
-            v1.neighbours.add(v2)
-            v2.neighbours.add(v1)
-        else:
-            # We didn't find an existing vertex for both items.
+    def get_similarity_score(self, item1: Any, item2: Any) -> float:
+        if item1 or item2 not in self._vertices:
             raise ValueError
+        else:
+            return self._vertices[item1].similarity_score(self._vertices[item2])
 
-
-# The recommend book algorithm
-def recommend_books(self, book: str, limit: int) -> list[str]:
-    books_reverse_alp = [vertex for vertex in self._vertices
-                         if self._vertices[vertex].kind == 'book']
-    books_reverse_alp.remove(book)
-    books_reverse_alp.sort(reverse=True)
-    simlrity_scr_map = []
-    # create a tuple for every book
-    for book2 in books_reverse_alp:
-        simlrity_scr_map.append((book2, self.get_similarity_score(book, book2)))
-    # sort the list in terms of similarity score
-    simlrity_scr_map.sort(key=lambda key: key[1], reverse=True)
-    # now add them to the result in a while loop
-    result, count, current_pair = [], 0, simlrity_scr_map.pop(0)
-    while count < limit and current_pair[1] != 0.0:
-        if all(pair[0] != current_pair[1] for pair in result):
-            result.append(current_pair[0])
-            current_pair = simlrity_scr_map.pop(0)
-            count += 1
-    return result
+    def recommend_locations(self, location: str, limit: int) -> list[str]:
+        locations_reverse_alp = [vertex for vertex in self._vertices
+                             if self._vertices[vertex].kind == 'location']
+        locations_reverse_alp.remove(location)
+        locations_reverse_alp.sort(reverse=True)
+        simlrity_scr_map = []
+        # create a tuple for every location
+        for location2 in locations_reverse_alp:
+            simlrity_scr_map.append((location2, self.get_similarity_score(location, location2)))
+        # sort the list in terms of similarity score
+        simlrity_scr_map.sort(key=lambda key: key[1], reverse=True)
+        # now add them to the result in a while loop
+        result, count, current_pair = [], 0, simlrity_scr_map.pop(0)
+        while count < limit and current_pair[1] != 0.0:
+            if all(pair[0] != current_pair[1] for pair in result):
+                result.append(current_pair[0])
+                current_pair = simlrity_scr_map.pop(0)
+                count += 1
+        return result
