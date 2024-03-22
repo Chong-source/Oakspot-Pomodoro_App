c172368ea79d04d9549f505c40a1569c2dbc5e6f
phuonganhdangamy <anh.dangphuong@mail.utoronto.ca>
2024-03-22 13:35:59 -0400

    Update pomodoro timer.py

--- .idea/pomodoro timer.py
+++ .idea/pomodoro timer.py
@@ -1,3 +1,8 @@
 from tkinter import *
 # https://www.youtube.com/watch?v=FpZ70RkUfNs
 import math
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text = '00:00')
+
