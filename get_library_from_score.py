3efad405f0db812ef294ff1b730b4ac1ba57744e
Chong <chongwan.w@gmail.com>
2024-03-22 11:58:25 -0400

    data class

--- get_library_from_score.py
+++ get_library_from_score.py
@@ -3,3 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class libraries:
     """A data class that stores the scores of each """
+    quiet: int
+    bright: int
+    charger: int
+    crowdedness: int
